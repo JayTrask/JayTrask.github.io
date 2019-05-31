c5214a3ae515d0cb0589a1694a5f10a7afa33d4e
JayTrask <jontrask@umass.edu>
2019-05-31 19:29:14 -0400

    initial commit; copy of portfolio project files

---
+++ src/components/Introduction.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import devPic from '../imgs/devPic.png'
+import headshot from '../imgs/headshot.gif'
+import headshot1 from '../imgs/headshot1.png'
+import headshot2 from '../imgs/headshot2.png'
+import enterButton from '../imgs/enterButton.png'
+
+
+class Introduction extends Component{
+
+	
+	render(){
+		return(
+			<div className="Introduction">
+				<div className="row Introduction">
+					<div className="intro-pad"></div>
+					<div className="intro-column">
+						<img src={headshot} className="headshot" ></img>
+					</div>
+
+					<div className="intro-column">
+						<img src={enterButton} className="enter-button"></img>
+					</div>
+					<div className="intro-pad"></div>
+				</div>
+
+			
+
+			</div> 
+		);
+	}
+}
+export default Introduction;
