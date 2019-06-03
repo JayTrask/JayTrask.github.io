c7877e9785a231447b0a03a292b23fe823be0c8f
JayTrask <jontrask@umass.edu>
2019-06-03 17:38:40 -0400

    file structure

---
+++ precache-manifest.06683e247758b761a26c1bcc068f43a4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66731bdd7949411855dfafdfbcf8f3c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edaf08b5125f24b06e97",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6581924ccd305f430d1d",
+    "url": "/static/css/main.3c43fa6e.chunk.css"
+  },
+  {
+    "revision": "edaf08b5125f24b06e97",
+    "url": "/static/js/2.439b5c6b.chunk.js"
+  },
+  {
+    "revision": "6581924ccd305f430d1d",
+    "url": "/static/js/main.438ddac3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4769d9f38f1c98c2a3078f6c2bd5c40e",
+    "url": "/static/media/CurrentResume.4769d9f3.pdf"
+  },
+  {
+    "revision": "4e1f3c06d5bb851a5ddce67a830de90a",
+    "url": "/static/media/aboutme1.4e1f3c06.png"
+  },
+  {
+    "revision": "7b6ebd6bcb9d44485c2bcb15dfe2512b",
+    "url": "/static/media/aboutme2.7b6ebd6b.png"
+  },
+  {
+    "revision": "45167c51acaee0d823b390937ec8fdec",
+    "url": "/static/media/aboutme3.45167c51.png"
+  },
+  {
+    "revision": "17af54378c6a4d3114f4fe7a048356dd",
+    "url": "/static/media/background.17af5437.png"
+  },
+  {
+    "revision": "3aa658a92ac8c3f7fdba8f81d4f64bf4",
+    "url": "/static/media/devPic.3aa658a9.png"
+  },
+  {
+    "revision": "f41e2503d554a72d09ac4a271de66b55",
+    "url": "/static/media/dubs.f41e2503.png"
+  },
+  {
+    "revision": "a4977653f18714a7bf6e38c621a0c3d0",
+    "url": "/static/media/enterButton.a4977653.png"
+  },
+  {
+    "revision": "b674eb81a298ea88ee6f639e7d0016e3",
+    "url": "/static/media/headshot.b674eb81.gif"
+  },
+  {
+    "revision": "1c1ffe7fa4aa9d70d7d89b41a15912e5",
+    "url": "/static/media/headshot1.1c1ffe7f.png"
+  },
+  {
+    "revision": "6c25146ae7f4f92886b1edaca019e454",
+    "url": "/static/media/headshot2.6c25146a.png"
+  },
+  {
+    "revision": "c1426e2e831a90550693260416515a39",
+    "url": "/static/media/homeIcon.c1426e2e.png"
+  },
+  {
+    "revision": "ff2175ee8131a40c22527a64db48368e",
+    "url": "/static/media/leetcode.ff2175ee.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4eeb2ac6f84c26548872907598641988",
+    "url": "/static/media/resumeIcon.4eeb2ac6.png"
+  },
+  {
+    "revision": "9a44627a92645865a199c87bc0904210",
+    "url": "/static/media/temp.9a44627a.png"
+  }
+]);
